88ec1c26a9535d3863dee151a0e55df91a1d60ce
OKrylov <krylov92oa@gmail.com>
2018-12-20 23:36:39 +0300

    Lesson 3

---
+++ Lesson3_Streams/src/objects/stream/Human.java
@@ -0,0 +1,18 @@
+package objects.stream;
+
+import java.io.Serializable;
+
+public class Human  implements Serializable {
+
+    public int age;
+
+    public Human() {
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                '}';
+    }
+}
